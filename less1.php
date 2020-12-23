da02605b028ca26b6770befd2d541f155995d51d
AnhDungPham2901 <dungpham290198@gmail.com>
2020-12-23 07:17:20 +0700

    modified 2 places

--- less1.php
+++ less1.php
@@ -3,10 +3,6 @@
     echo "added new lines";
     echo "add to check";
 
-    // Check git Again: Dung
-    // Add new line 123: PI
-    // Add new line : PI
-
-    // Add new line to conflict 
+    // Conflict file nay ne
 ?>
 
